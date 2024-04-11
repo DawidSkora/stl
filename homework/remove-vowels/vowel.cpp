5b946b106dc91b5ada18a5c7c037c68f7ca435a2
DawidSkora <dawid.skora26@gmail.com>
2024-04-11 20:42:11 +0200

    remove vowels task

---
+++ homework/remove-vowels/vowel.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <string>
+
+#include "vowel.hpp"
+
+void removeVowels(std::vector<std::string>& v) {
+    for(auto& word : v) {
+        word.erase(std::remove_if(word.begin(), word.end(), [](char x) {
+            return x == 'a' || x == 'e' || x == 'o' || x == 'u' || x == 'i' || x == 'y' ||
+                   x == 'A' || x == 'E' || x == 'O' || x == 'U' || x == 'I' || x == 'Y';
+        }), word.end());
+    }
+}
