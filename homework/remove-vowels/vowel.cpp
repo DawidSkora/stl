04cc80b8bfa08d68ba9992861ee068b8c02f72fb
DawidSkora <dawid.skora26@gmail.com>
2024-04-13 14:41:27 +0200

    Format code

--- homework/remove-vowels/vowel.cpp
+++ homework/remove-vowels/vowel.cpp
@@ -1,13 +1,14 @@
-#include <vector>
 #include <string>
+#include <vector>
 
 #include "vowel.hpp"
 
 void removeVowels(std::vector<std::string>& v) {
-    for(auto& word : v) {
+    for (auto& word : v) {
         word.erase(std::remove_if(word.begin(), word.end(), [](char x) {
-            return x == 'a' || x == 'e' || x == 'o' || x == 'u' || x == 'i' || x == 'y' ||
-                   x == 'A' || x == 'E' || x == 'O' || x == 'U' || x == 'I' || x == 'Y';
-        }), word.end());
+                       return x == 'a' || x == 'e' || x == 'o' || x == 'u' || x == 'i' || x == 'y' ||
+                              x == 'A' || x == 'E' || x == 'O' || x == 'U' || x == 'I' || x == 'Y';
+                   }),
+                   word.end());
     }
 }
